b89c70523c9b8dc5821b05b7e32af5b03ad72619
Kai <>
2023-12-21 20:24:33 +0900

    add checks for arguments, add more math replacements

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ if __name__ == '__main__':
     parser.add_argument('input_file', type=str, help='input file path')
     parser.add_argument('-o', '--output_path', type=str, help='output file path', default=None)
     args = parser.parse_args()
+    if not os.path.isfile(args.input_file):
+        print(f'Input file {args.input_file} does not exist.')
+        exit(1)
 
     out_path = args.output_path or os.path.dirname(args.input_file)
-    os.system('nougat "{}" -o "{}" -m 0.1.0-base'.format(args.input_file, out_path))
+    exit_code = os.system('nougat "{}" -o "{}" -m 0.1.0-base'.format(args.input_file, out_path))
+    if exit_code != 0:
+        print('Failed to convert pdf to markdown. Please check your nougat installation.')
+        exit(1)
 
     mp3_gen = MP3Generator(os.path.join(out_path, os.path.basename(args.input_file).replace('.pdf', '.mmd')))
     mp3_files = mp3_gen.generate_mp3_files()
